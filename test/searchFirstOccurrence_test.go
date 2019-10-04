fb2a722e0c9fea57571d42bc4beb2a6895775188
fbonhomm <flo-github@outlook.fr>
2019-10-04 19:10:57 +0200

    test: add test for searchFirstOccurrence function

---
+++ test/searchFirstOccurrence_test.go
@@ -0,0 +1,38 @@
+package test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"github.com/fbonhomm/Knuth-Morris-Pratt-Algorithm/kmp"
+)
+
+var _ = Describe("SearchFirstOccurrence", func() {
+	Context("Success", func() {
+		It("with \"abc abcdab abcdabcdabde\" as buffer and \"abcdabd\" as pattern", func() {
+			// Prepare
+			var index int
+			var buffer = []byte("abc abcdab abcdabcdabde")
+			var pattern = []byte("abcdabd")
+
+			// Process
+			index = kmp.SearchFirstOccurrence(buffer, pattern)
+
+			// Expect
+			Expect(index).To(Equal(15))
+		})
+
+		It("with \"abc abcdab abcdabcdabde\" as buffer and \"abcdabdr\" as pattern", func() {
+			// Prepare
+			var index int
+			var buffer = []byte("abc abcdab abcdabcdabde")
+			var pattern = []byte("abcdabdr")
+
+			// Process
+			index = kmp.SearchFirstOccurrence(buffer, pattern)
+
+			// Expect
+			Expect(index).To(Equal(-1))
+		})
+	})
+})
